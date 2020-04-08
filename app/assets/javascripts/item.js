fdbd2917caa800223d2066e6c041f267e8179166
fumito0808ajgj <42643727+fumito0808ajgj@users.noreply.github.com>
2020-04-08 13:33:43 +0900

    fix item-is

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -1,44 +1,50 @@
 $(document).on('turbolinks:load', ()=> {
-  // 画像用のinputを生成する関数
+  // console.log("hello")
   const buildFileField = (index)=> {
     const html = `<div data-index="${index}" class="js-file_group">
                     <input class="item-image-title-box-input-1-d" type="file"
                     name="item[images_attributes][${index}][image]"
                     id="item_images_attributes_${index}_image"><br>
                     <div class="js-remove">削除</div>
-                  </div>`;
+                    </div>`;
     return html;
   }
+  
 
   const buildImg = (index, url)=> {
     const html = `<img data-index="${index}" src="${url}" width="100px" height="100px">`;
     return html;
   }
-
-  // file_fieldのnameに動的なindexをつける為の配列
-  let fileIndex = [1,2,3,4,5,6,7,8,9,10];
+  
+  let fileIndex = [1,2,3,4,5,6,7,8,9,10];    
 
   $('#image-box').on('change', '.item-image-title-box-input-1-d', function(e) {
-    // fileIndexの先頭の数字を使ってinputを作る
+    
     const targetIndex = $(this).parent().data('index');
-    // ファイルのブラウザ上でのURLを取得する
     const file = e.target.files[0];
     const blobUrl = window.URL.createObjectURL(file);
     
     if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
       img.setAttribute('src', blobUrl);
     } else {  // 新規画像追加の処理
+      var index = $("#previews").children()["length"] + 1;
       $('#previews').append(buildImg(targetIndex, blobUrl));
+      $('#image-box').append(buildFileField(index));
 
-    $('#image-box').append(buildFileField(fileIndex[0]));
-    fileIndex.shift();
-    // 末尾の数に1足した数を追加する
-    fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
+      $('label.item-image-title-box-input-1').attr("for", `item_images_attributes_${index}_image`)
+      $('.item-image-title-box-input-1-text').text('')
+      $('.js-remove').remove()
+
+
+      fileIndex.shift();
+      fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
+    }
   });
 
   $('#image-box').on('click', '.js-remove', function() {
     $(this).parent().remove();
-    // 画像入力欄が0個にならないようにしておく
+    $(`img[data-index="${targetIndex}"]`).remove();
+
     if ($('.item-image-title-box-input-1-d').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
   });
 });
--- db/migrate/20200402145151_create_images.rb
+++ db/migrate/20200402145151_create_images.rb
@@ -2,7 +2,7 @@ class CreateImages < ActiveRecord::Migration[5.2]
   def change
     create_table :images do |t|
       t.bigint "item_id", null: false 
-      t.string :image, null: false|
+      t.string :image, null: false
       t.timestamps
     end
   end
