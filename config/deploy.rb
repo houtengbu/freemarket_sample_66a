d0a7b2b26daabdf3ae9edd2a0f7afda1a35c2476
fumito0808ajgj <42643727+fumito0808ajgj@users.noreply.github.com>
2020-04-15 16:31:34 +0900

    fix deploy.rb peoduction.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,7 @@ set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/team66-a.pem'] 
+                  keys: ['~/.ssh/team-66a.pem'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
