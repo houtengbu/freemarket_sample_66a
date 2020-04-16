60d580f1e2e119ee8732ac609e879c4fdc539faf
fumito0808ajgj <42643727+fumito0808ajgj@users.noreply.github.com>
2020-04-16 10:59:32 +0900

    fix carrierwave.rb

--- db/schema.rb
+++ db/schema.rb
@@ -48,10 +48,8 @@ ActiveRecord::Schema.define(version: 2020_04_15_082653) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
-    t.string "ancestry"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -63,7 +61,6 @@ ActiveRecord::Schema.define(version: 2020_04_15_082653) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "buyer_id"
-    t.bigint "user_id", null: false
     t.string "name", null: false
     t.text "text", null: false
     t.string "status", null: false
@@ -73,9 +70,9 @@ ActiveRecord::Schema.define(version: 2020_04_15_082653) do
     t.integer "selling_price", null: false
     t.bigint "category_id", null: false
     t.integer "price", null: false
-    t.string "image", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image", null: false
     t.bigint "saler_id"
     t.string "brand"
   end
