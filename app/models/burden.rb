11d48fc9854ada41019bb28acdcb35c88360b35b
fumito0808ajgj <42643727+fumito0808ajgj@users.noreply.github.com>
2020-04-22 21:52:02 +0900

    fix new haml-formwith

--- app/models/burden.rb
+++ app/models/burden.rb
@@ -1,8 +1,8 @@
 class Burden < ActiveHash::Base
-    self.data = [
-        {id: 1, name: "選択してください"}, {id: 2, name: "未定"}, {id: 3, name: "ゆうメール"},
-        {id: 4, name: "レターパック"}, {id: 5, name: "普通郵便(定形、定形外)"}, {id: 6, name: "クロネコヤマト"},
-        {id: 7, name: "ゆうパック"}, {id: 8, name: "クリックポスト"}, {id: 9, name: "ゆうパケット"}   
+  self.data = [
+    {id: 1, name: "選択してください"}, {id: 2, name: "未定"}, {id: 3, name: "ゆうメール"},
+    {id: 4, name: "レターパック"}, {id: 5, name: "普通郵便(定形、定形外)"}, {id: 6, name: "クロネコヤマト"},
+    {id: 7, name: "ゆうパック"}, {id: 8, name: "クリックポスト"}, {id: 9, name: "ゆうパケット"}   
 
     ]
 end
