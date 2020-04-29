82a9d5486803f8fb101e1538fa35c3504a0e1bf7
fumito0808ajgj <42643727+fumito0808ajgj@users.noreply.github.com>
2020-04-29 21:05:43 +0900

    put together:category_id, :status_id, :burden_id, :area_id, :days_to_ship_id,

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,8 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   validates :name, presence: true, length: { maximum: 40 }
-  validates :category_id, presence: true
-  validates :status_id, presence: true
+  validates :category_id, :status_id, :burden_id, :area_id, :days_to_ship_id, presence: true
   validates :text, presence: true, length: { maximum: 1000 }
-  validates :burden_id, presence: true
-  validates :area_id, presence: true
-  validates :days_to_ship_id, presence: true
   validates :selling_price, presence: true, numericality: { greater_than: 300, less_than:9999999 }
   has_many :images
   has_one :buyer
