0345bdf9444cc3952571ca3b6c4bb479a935f840
fumito0808ajgj <42643727+fumito0808ajgj@users.noreply.github.com>
2020-05-05 14:23:48 +0900

    add migrationfile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_26_160119) do
+ActiveRecord::Schema.define(version: 2020_05_05_051216) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
