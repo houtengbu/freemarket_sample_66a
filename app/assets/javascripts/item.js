0469defe6a1ae679fc35006d4a11e7a621b542d0
Masa6190 <masamitsu.nakayama1@gmail.com>
2020-05-05 23:41:19 +0900

    一時保存

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -1,95 +1,109 @@
-$(document).on('turbolinks:load', ()=> {
-  const buildFileField = (num)=> {
-    const html = `<div data-index="${num}" class="js-file_group">
-                    <input class="js-file" type="file"
-                    name="item[images_attributes][${num}][image]"
-                    id="item_images_attributes_${num}_image"><br>
-                  </div>`;
-    return html;
-  }
-
-  const dele = ()=> {
-    const html = `<div class="js-remove" id="aaaa">削除</div>
-                  </div>`;
-    return html;
-  }
-
-  const buildImg = (index, url)=> {
-    const html = `<img data-index="${index}" src="${url}" width="100px" height="100px" id="test">
-    <div class="js-remove">削除</div>`;
-    console.log(html)
-    return html;
-  }
-
-  let fileIndex = [1,2,3,4,5,6,7,8,9,10];
-  lastIndex = $('.js-file_group:last').data('index');
-  fileIndex.splice(0, lastIndex);
   
-  $('.hidden-destroy').hide();
-  
-  $('#image-box').on('change', '.js-file', function(e) {
-    const targetIndex = $(this).parent().data('index');
-    const file = e.target.files[0];
-    console.log(file)
-    const blobUrl = window.URL.createObjectURL(file);
-    console.log(blobUrl)
-    
-    //すでに選んであるファイルを変更した場合
-    if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
-      img.setAttribute('image', blobUrl);
-      // 新規投稿した際にプレビューidの下に画像を入れ、イメージボックスidの下に新しいファイルフィールドを作る。そのせいで画像の下ではなく真ん中の下にファイルを選択してくださいとでる。やりたいのは画像の下に削除を移動したい
-    } else {
-      $('#previews').append(buildImg(targetIndex, blobUrl));
-      // $('#test').append(dele);
-      $('#image-box').append(buildFileField(fileIndex[0]));
-      console.log(fileIndex);
-
-      $('label.item-image-title-box-input-1').attr("for", `item_images_attributes_${targetIndex}_image`)
-      $('.item-image-title-box-input-1-text').text('')
-      // $('.js-remove').remove()
-      
-      fileIndex.shift();
-      fileIndex.push(fileIndex[fileIndex.length - 1] + 1);
-    }
-  });
-  
-  $('#image-box').on('click', '.js-remove', function() {
-    const targetIndex = $(this).parent().data('index');
-    const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
-    if (hiddenCheck) hiddenCheck.prop('checked', true);
-    
-    $(this).parent().remove();
-    $(`img[data-index="${targetIndex}"]`).remove();
-    
-    if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
-  });
-
-
+$(document).on('turbolinks:load', function() {
   $(function(){
-    $('#price_calc').on('input', function(){   //リアルタイムで表示したいのでinputを使う｡入力の度にイベントが発火するようになる｡
-      var data = $('#price_calc').val(); // val()でフォームのvalueを取得(数値)
-      var profit = Math.round(data * 0.9)  // 手数料計算を行う｡dataにかけているのが0.9なのは単に引きたい手数料が10%のため｡
-      var fee = (data - profit) // 入力した数値から計算結果(profit)を引く｡それが手数料となる｡
-      $('.right_bar').html(fee) //  手数料の表示｡html()は追加ではなく､上書き｡入力値が変わる度に表示も変わるようにする｡
-      $('.right_bar').prepend('¥') // 手数料の前に¥マークを付けたいので
-      $('.right_bar_2').html(profit)
-      $('.right_bar_2').prepend('¥')
-      $('#price').val(profit) // 計算結果を格納用フォームに追加｡もし､入力値を追加したいのなら､今回はdataを引数に持たせる｡
-      if(profit == '') {   // もし､計算結果が''なら表示も消す｡
-      $('.right_bar_2').html('');
-      $('.right_bar').html('');
+
+    function buildHTML(count) {     
+      var html = `<div class = "preview-box" id ="preview-box__${count}">
+                    <div class="upper-box">
+                      <img src="" alt="preview">
+                    </div>
+                    <div class = "lower-box">
+                      <div class = "delete-box", id ="delete_btn_${count}">
+                        <span>削除</span>
+                      </div>
+                    </div>
+                  </div>`
+      return html;
+    }
+    // 投稿編集時
+    if (window.location.href.match(/\/items\/\d+\/edit/)){
+      //登録済み画像のプレビュー表示欄の要素を取得する
+      var prevContent = $('.label-content').prev();
+      labelWidth = (770 - $(prevContent).css('width').replace(/[^0-9]/g, ''));
+      $('.label-content').css('width', labelWidth);
+      //プレビューにidを追加
+      $('.preview-box').each(function(index, box){
+        $(box).attr('id', `preview-box__${index}`);
+      })
+      //削除ボタンにidを追加
+      $('.delete-box').each(function(index, box){
+        $(box).attr('id', `delete_btn_${index}`);
+      })
+      var count = $('.preview-box').length;
+      //プレビューが5あるときは、投稿ボックスを消しておく
+      if (count == 5) {
+        $('.label-content').hide();
       }
-    })
-  })
+    }
 
-$(".item-image-title-box-input").on("click",function() {
-  console.log("test")
-  const test = $(".js-file:last");
-  test.trigger("click");
-})
+    function setLabel() {
+      var prevContent = $('.label-content').prev();
+      labelWidth =(770 - $(prevContent).css('width').replace(/[^0-9]/g, ''));
+      $('.label-content').css('width', labelWidth);
+    }
+    // プレビューの追加
+    $(document).on('change', '.hidden-field', function() {
+      setLabel();
 
+      var id = $(this).attr('id').replace(/[^0-9]/g, '');
+      $('.label-box').attr({id: `label-box--${id}`,for: `item_images_attributes_${id}_image`});
+      var file = this.files[0];
+      var reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = function() {
+        var src = this.result;
+        if ($(`#preview-box__${id}`).length == 0) {
+          var count = $('.preview-box').length;
+          var html = buildHTML(id);
+          var prevContent = $('.label-content').prev();
+          $(prevContent).append(html);
+        }
+
+        $(`#preview-box__${id} img`).attr('src', `${src}`);
+        var count =$('.preview-box').length;
+        if (count == 5) {
+          $('.label-content').hide();
+        }
+        
+        if ($(`#item_images_attributes_${id}__destroy`)){
+          $(`#item_images_attributes_${id}__destroy`).prop('checked',false);
+        } 
+
+        setLabel();
+        if(count < 5){
+          $('.label-box').attr({id: `label-box--${count}`, for: `item_images_attributes_${count}_image`});
+        }
+      }
+    });
+    
+    $(document).on('click', '.delete-box', function() {
+      var count = $('.preview-box').length;
+      setLabel(count);
+      var id = $(this).attr('id').replace(/[^0-9]/g, '');
+      $(`#preview-box__${id}`).remove();
+
+      //新規投稿時
+      if ($(`#item_images_attributes_${id}__destroy`).length == 0) {
+        $(`#item_images_attributes_${id}_image`).val("");
+        var count = $('.preview-box').length;
+        if (count == 4) {
+          $('.label-content').show();
+        }
+        setLabel(count);
+        if(id < 5){
+          $('.label-box').attr({id: `label-box--${id}`, for: `item_images_attributes_${id}_image`});
+        }
+      } else {
+        //投稿編集
+        $(`#item_images_attributes_${id}__destroy`).prop('checked', true);
+        if (count ==4) {
+          $('.label-content').show();
+        }
+        setLabel();
+        if(id < 5){
+          $('.label-box').attr({id: `label-box--${id}`, for: `item_images_attributes_${id}_image`});
+        }  
+      }
+    });
+  });
 });
-
-
-
-
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,6 @@ class Item < ApplicationRecord
   validates :category_id, :status_id, :burden_id, :area_id, :days_to_ship_id, presence: true
   validates :text, presence: true, length: { maximum: 1000 }
   validates :selling_price, presence: true, numericality: { greater_than: 300, less_than:9999999 }
-  has_many :images, dependent: :destroy
   has_one :buyer
   belongs_to :category
   belongs_to :user, optional: true
@@ -14,5 +13,16 @@ class Item < ApplicationRecord
   belongs_to_active_hash :burden
   belongs_to_active_hash :days_to_ship
   belongs_to_active_hash :status
+
+  has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
+
+  def previous
+    Item.where("id < ?", self.id).order("id DESC").first
+  end
+
+  def next
+    Item.where("id > ?", self.id).order("id ASC").first
+  end
+  
 end
