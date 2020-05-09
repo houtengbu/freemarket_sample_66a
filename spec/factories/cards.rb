ed370dcaca7ea241ea89a430c768f7ac3a88d07c
hk1256kt <60028619+hk1256kt@users.noreply.github.com>
2020-05-09 18:37:34 +0900

    buyers-sever-side-5

--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
 
   factory :card do
     user_id            {"1"}
-    customer_id        {"acc"}
+    customer_id        {"abc"}
     card_id            {"abc"}
   end
 
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -2,20 +2,28 @@ require 'rails_helper'
 
 describe Card do
   describe '#create' do
+
     # 1
+    it "user_id,customer_id,card_idが存在すれば登録できること" do
+      card = build(:card)
+      expect(card).to be_valid
+    end
+
+    # 2
     it "user_idがない場合は登録できないこと" do
       card = build(:card, user_id: nil)
       card.valid?
       expect(card.errors[:user_id]).to include("can't be blank")
     end
 
-    # 2
+    # 3
     it "customer_idがない場合は登録できないこと" do
       card = build(:card, customer_id: nil)
       card.valid?
       expect(card.errors[:customer_id]).to include("can't be blank")
     end
-    # 3
+    
+    # 4
     it "card_idがない場合は登録できないこと" do
       card = build(:card, card_id: nil)
       card.valid?
