ddee482171a2fc713cd4d511de8fb212b3f35a71
Masa6190 <masamitsu.nakayama1@gmail.com>
2020-05-09 23:38:50 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# 10.times do
-#   User.create(
-#      nickname: Faker::DragonBall.character,
-#      password: Faker::Internet.password,
-#      email: Faker::Internet.email
-#    )
-# end
-
 # レディース
 lady = Category.create(name: "レディース")
 lady_1 = lady.children.create(name: "トップス")
