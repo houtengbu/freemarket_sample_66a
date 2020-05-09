f7598b71f4fde008c62c12fa3f43f13c7770666e
houtengbu <houtengbu@gmail.com>
2020-05-09 20:10:39 +0900

    Revert "バリデーション"
    This reverts commit 6f05069caabb886a98dbddf2295757bcc771a3fe.

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,19 +1,3 @@
 class Address < ApplicationRecord
-  VALID_ZENKAKU_REGEX = /\A[０-９]+\z/
-  VALID_KATAKANA_REGEX = /\A[\p{katakana}\p{blank}ー－]+\z/
-
   belongs_to :user
-  validates :familyname, presence: true, format: { with: VALID_ZENKAKU_REGEX, message: 'は全角で入力して下さい'}
-  validates :firstname, presence: true, format: { with: VALID_ZENKAKU_REGEX, message: 'は全角で入力して下さい'}
-  validates :familyname_kana, presence: true, format: { with: VALID_KATAKANA_REGEX, message: 'はカタカナで入力して下さい'}
-  validates :firstname_kana, presence: true, format: { with: VALID_KATAKANA_REGEX, message: 'はカタカナで入力して下さい'}
-  validates :birth_year, presence: true  
-  validates :birth_month, presence: true
-  validates :birth_day, presence: true
-  validates :postal_code, presence: true
-  validates :prefecture, presence: true
-  validates :city, presence: true
-  validates :block, presence: true
 end
-
-
