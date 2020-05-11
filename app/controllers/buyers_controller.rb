9d7b8b2455be976cce8d13f308a3c123f1124efd
Masa6190 <masamitsu.nakayama1@gmail.com>
2020-05-11 20:33:51 +0900

    詳細から購入画面に行く際にフラッシュメッセージを追加

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -5,7 +5,7 @@ class BuyersController < ApplicationController
 
   def index
     if @card.blank?                                                       #登録された情報がない場合にカード登録画面に移動
-      redirect_to new_card_path
+      redirect_to new_card_path, notice:"お支払い方法を登録してください"
     else
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]                            #保管した顧客IDでpayjpから情報取得
       customer = Payjp::Customer.retrieve(@card.customer_id)              #保管したカードIDでpayjpから情報取得、
