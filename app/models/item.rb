3b1fb1163efeff01838117dbe52c415bb8f8d185
Masa6190 <masamitsu.nakayama1@gmail.com>
2020-05-16 11:31:04 +0900

    item.rbをscopeに変更＆検索結果の文言追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,9 +17,6 @@ class Item < ApplicationRecord
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
 
-  def self.search(search)
-    return Item.all unless search
-    Item.where('text LIKE(?)', "%#{search}%")
-  end
+  scope :search, -> (search){ where('text LIKE(?)', "%#{search}%") }
 
 end
